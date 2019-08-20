e0c719dcf9c7b8a103863e2f0a671add78498bbd
James Cuénod <j3frea+gitkraken@gmail.com>
2019-08-19 21:56:15 -0500

    Changed output path

--- helpers/2. enrich-ccat-to-lxxdb.js
+++ helpers/2. enrich-ccat-to-lxxdb.js
@@ -74,11 +74,11 @@ fs.readdir(mlxxRoot, (err, filenames) => {
 		}
 	})
 	if (fails.length > 0) {
-		fs.writeFileSync("output/failed-enrichments.json", JSON.stringify(fails, null, 2), 'utf8')
+		fs.writeFileSync("../output/failed-enrichments.json", JSON.stringify(fails, null, 2), 'utf8')
 		console.log("There were some fails:", fails.length)
 	}
 	if (missingDefs.length > 0) {
-		fs.writeFileSync("output/failed-defs.json", JSON.stringify(missingDefs, null, 2), 'utf8')
+		fs.writeFileSync("../output/failed-defs.json", JSON.stringify(missingDefs, null, 2), 'utf8')
 		console.log("There were missing defs:", missingDefs.length)
 	}
 
@@ -88,14 +88,14 @@ fs.readdir(mlxxRoot, (err, filenames) => {
 	}
 	console.log("Completion...")
 	completeProcess()
-	console.log("Writing data/allwords.json")
-	fs.writeFileSync("./data/allwords.json", JSON.stringify(allWords, null, 2), 'utf8')
+	console.log("Writing ../output/allwords.json")
+	fs.writeFileSync("../output/allwords.json", JSON.stringify(allWords, null, 2), 'utf8')
 })
 
 
 const exceptions = {
-	"Lam": {ch: 0, v: 1},
-	"Bel": {ch: 1, v: 1}
+	"Lam": { ch: 0, v: 1 },
+	"Bel": { ch: 1, v: 1 }
 }
 
 let fails = []
@@ -103,97 +103,116 @@ let fails = []
 let count = 0
 const parseFile = (lines, filename) => {
 	// let bulkInsertCounter = 0
-	let book_name = ""
-	let chapter_nr = 0
-	let verse_nr = 0
-	let word_nr = -1
+	let book = ""
+	let chapter = 0
+	let verse = 0
+	let word_in_verse = -1
+	let word_in_verse_offset = 0
 	for (i in lines) {
 		line = lines[i].trim()
 		if (!line) continue
 		if (+i === 5558 && filename === "DanielOG") {
 			// Dan 5:0
-			book_name = "Dan"
-			chapter_nr = 5
-			verse_nr = 0
-			word_nr = -1
+			book = "Dan"
+			chapter = 5
+			verse = 0
+			word_in_verse = -1
+			word_in_verse_offset = 0
 		}
 		else if (line === "Bel 31/32") {
-			verse_nr = 31 //That's what it is in the lemmas file
-			word_nr = -1
+			verse = 31 //That's what it is in the lemmas file
+			word_in_verse = -1
+			word_in_verse_offset = 0
 		}
 		else if (line === "Bel 15-17") {
-			verse_nr = 15 //That's what it is in the lemmas file
-			word_nr = -1
+			verse = 15 //That's what it is in the lemmas file
+			word_in_verse = -1
+			word_in_verse_offset = 0
 		}
 		else if (/^\w+$/.test(line)) { // Bel (some files start on v1, some on v0)
-			book_name = line
-			if (filename === "PsSol")
-				chapter_nr++
+			book = line
+			if (filename === "PsSol" || filename === "Odes")
+				chapter++
 			else
-				chapter_nr = 1
-			verse_nr = 0
+				chapter = 1
+			verse = 0
 			if (Object.keys(exceptions).includes(line)) {
 				const k = Object.keys(exceptions).find(x => x === line)
-				chapter_nr = exceptions[k]["ch"]
-				verse_nr = exceptions[k]["v"]
+				chapter = exceptions[k]["ch"]
+				verse = exceptions[k]["v"]
 			}
-			word_nr = -1
+			word_in_verse = -1
+			word_in_verse_offset = 0
 		}
 		else if (/.+ .+:.+/.test(line)) { // Ruth 1:1
 			const lineParts = line.split(/[\s|:]/)
-			book_name = lineParts[0]
-			chapter_nr = lineParts[1]
-			verse_nr = lineParts[2]
-			word_nr = -1
+			book = lineParts[0]
+			chapter = lineParts[1]
+			verse = lineParts[2]
+			word_in_verse = -1
+			word_in_verse_offset = 0
 		}
 		else if (/.+ [1-9]+/.test(line)) { // Obad 1 (books with 1 chapter)
 			const lineParts = line.split(/\s/)
-			book_name = lineParts[0]
-			chapter_nr = 1
-			verse_nr = lineParts[1]
-			word_nr = -1		
+			book = lineParts[0]
+			chapter = 1
+			verse = lineParts[1]
+			word_in_verse = -1
+			word_in_verse_offset = 0
 		}
 		else {
-			word_nr++
+			use_lemmas = true
+			word_in_verse++
 			count++
 			word = bc.betaCodeToGreek(line.substring(0, 25).trim())
 			old_root = bc.betaCodeToGreek(line.substring(36).replace(/\s+/, ' '))
 			real_lemma = bc.betaCodeToGreek(line.substring(36).replace(/\s+/, ' '))
 			lemma_key = bc.betaCodeToGreek(line.substring(36).replace(/\s+/, ' '))
-			morph = line.substring(25,36).trim().replace(/\s+/, ' ')
+			morph = line.substring(25, 36).trim().replace(/\s+/, ' ')
 
 			let primaryKey = filename
 			if (!lemmas[primaryKey])
 				primaryKey = filenameMap[filename]
-			
+
 			if (!primaryKey) {
 				console.log("oh no", filename)
 			}
-				
-			let secondaryKey = `${book_name}.${chapter_nr}.${verse_nr}`
+
+			let secondaryKey = `${book}.${chapter}.${verse}`
 			if (!lemmas[primaryKey][secondaryKey])
-				secondaryKey = `${bookMap[book_name]}.${chapter_nr}.${verse_nr}`
+				secondaryKey = `${bookMap[book]}.${chapter}.${verse}`
 
 			let extradetails = {}
 			if (!lemmas[primaryKey][secondaryKey]) {
-				fails.push(`(${filename})[${primaryKey}] (${book_name})[${secondaryKey}] [${word_nr}] + ${i}:${word}`)
+				fails.push(`(${filename})[${primaryKey}] (${book})[${secondaryKey}] [${word_in_verse}] + ${i}:${word}`)
 				oth = null
 				return
 			}
 			else {
-				extradetails = lemmas[primaryKey][secondaryKey][word_nr]
+				extradetails = lemmas[primaryKey][secondaryKey][word_in_verse + word_in_verse_offset]
 			}
 
 			if (!extradetails || !extradetails.hasOwnProperty("lemma")) {
-				console.log(primaryKey, secondaryKey, word_nr, word)
+				console.log(primaryKey, secondaryKey, word_in_verse, word)
+			}
+			if (names_in_hosea.includes(line)) {
+				console.log(line)
+				console.log(chapter, verse, word, old_root)
+				word_in_verse_offset += line.substring(0, 25).split("-").length - 1
+				let lemma = old_root[0].toUpperCase() + old_root.replace(/\ /g, "-").slice(1)
+				extradetails = {
+					lemma,
+					key: lemma
+				}
 			}
 			const wordDetails = {
 				id: count,
-				book_name,
-				chapter_nr,
-				verse_nr,
-				word_nr,
+				word_in_verse,
+				book,
+				chapter,
+				verse,
 				word,
+				trailer: " ",
 				old_root,
 				real_lemma: extradetails.lemma,
 				lemma_key: extradetails.key,
@@ -325,11 +344,11 @@ const completeProcess = () => {
 	const fileOutput = sqlFile + 
 						paginateArray(allWords, 999).map(wordChunk => insertValues(wordChunk)).join("")
 	console.log(" - writing sql file")
-	fs.writeFileSync("./output/EnrichedContent.sql", fileOutput, 'utf8')
+	fs.writeFileSync("../output/EnrichedContent.sql", fileOutput, 'utf8')
 	console.log(" - importing to sqlite")
 	console.log("     (Sorry, this will take a while: like > 10min.")
 	console.log("      And there's not going to be any indication")
 	console.log("      it's succeeding so just trust me and wait)")
-	let stdout = execSync('sqlite3 output/lxx.db < output/EnrichedContent.sql')
+	let stdout = execSync('sqlite3 ../output/lxx.db < ../output/EnrichedContent.sql')
 	console.log("Done")
 }
